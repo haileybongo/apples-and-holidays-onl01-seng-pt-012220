194fce779efcba3f00fd5454e28df05fd1d188c3
Hailey Bongo <haileyebongo@gmail.com>
2020-02-03 18:38:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash_
+  holiday_hash.each do |season,data|
+    if season == :winter
+      
 
 end
 
