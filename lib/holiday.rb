88246ec2efa6871f707a7387136921e3a98fd43d
Hailey Bongo <haileyebongo@gmail.com>
2020-02-03 19:16:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, data|
     data.each do |holiday, supplies|
-      supplies.any? 
+      supplies.inclue? ("BB@")
+      puts holiday 
 
 end
 
